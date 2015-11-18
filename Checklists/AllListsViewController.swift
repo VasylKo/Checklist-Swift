5e9701a2d0cd77749107f774db38e0af86c67067
Vasyl Kotsiuba <clic@ukr.net>
2015-11-18 12:07:56 +0200

    added MVC structure. Added icon picker VC

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -62,6 +62,7 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
             cell.detailTextLabel!.text = "\(count) Remaining"
         }
 
+        cell.imageView!.image = UIImage(named: checklist.iconName)
         return cell
     }
     
---
+++ Checklists/IconPickerViewController.swift
@@ -0,0 +1,34 @@
+//
+//  IconPickerViewController.swift
+//  Checklists
+//
+//  Created by Vasyl Kotsiuba on 11/17/15.
+//  Copyright © 2015 Vasyl Koysiuba. All rights reserved.
+//
+
+import UIKit
+
+protocol IconPickerViewControllerDelegate: class {
+    func iconPicker(picker:IconPickerViewController, didPickIcon iconName: String)
+}
+
+class IconPickerViewController: UITableViewController {
+    weak var delegate: IconPickerViewControllerDelegate?
+    let icons = [ "No Icon", "Appointments", "Birthdays", "Chores", "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(
+            "IconCell", forIndexPath: indexPath)
+        
+        let iconName = icons[indexPath.row]
+        cell.textLabel!.text = iconName
+        cell.imageView!.image = UIImage(named: iconName)
+        
+        return cell
+    }
+    
+}
